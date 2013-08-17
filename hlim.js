a2fcd044b4ee4c2b9ec4f2a7ae8aaa272f57be96
Anthony Dee <bobhoward72@gmail.com>
2013-08-16 21:03:37 -0700

    Adds ability to posterize image (reduce number of colors) for massive speed boosts.

--- hlim.js
+++ hlim.js
@@ -8,6 +8,7 @@
  * config */
 var classPrefix = 'hlim-color-';
 var defaultWidth = 30;
+var defaultNumColors = -1;
 
 /*********************
  * working variables */
@@ -49,10 +50,19 @@ function turnHlimElementsIntoImages(cssRules) {
 		var text = elements[ai];
 		var textImgSrc = text.getAttribute('data-hlim-src');
 		var charsPerLine = text.getAttribute('data-hlim-width') || defaultWidth;
+		var maxColors = text.getAttribute('data-hlim-max-colors') || defaultNumColors;
 		var saveFilePrompt = text.getAttribute('data-hlim-save') == '';
 		text.style.fontFamily = 'monospace';
 
-		getPixelsFromImage(textImgSrc, charsPerLine, 0.5, (function(text_, charsPerLine_, saveFilePrompt_, cssRules_) {
+		//posterizes the image if the user wants to limit the number of colors in the image
+		var imageModifier = (maxColors < 0) ? null : (function(maxColors_) {
+			return function(data) {
+				return posterizeImage(data, Math.pow(maxColors_, 0.3333333));
+			};
+		})(maxColors);
+
+		
+		getPixelsFromImage(textImgSrc, charsPerLine, (function(text_, charsPerLine_, saveFilePrompt_, cssRules_) {
 			return function(data, timeTakenToLoadImage) {
 				var htmlStartingTime = new Date().getTime();
 				var colorSpans = textToHighlightImage(data, text_.innerHTML, charsPerLine_);
@@ -82,7 +92,7 @@ function turnHlimElementsIntoImages(cssRules) {
 					text_.innerHTML = colorSpans;
 				}
 			};
-		})(text, charsPerLine, saveFilePrompt, cssRules));
+		})(text, charsPerLine, saveFilePrompt, cssRules), 0.5, imageModifier);
 	}
 }
 
@@ -126,12 +136,19 @@ function loadDataForAllImages(callback) {
 		var text = elements[ai];
 		var textImgSrc = text.getAttribute('data-hlim-src');
 		var textImgWidth = text.getAttribute('data-hlim-width') || defaultWidth;
+		var maxColors = text.getAttribute('data-hlim-max-colors') || defaultNumColors;
 
-		getPixelsFromImage(textImgSrc, textImgWidth, 0.5, function(data) { //get its image
+		var imageModifier = (maxColors <= 0) ? null : (function(maxColors_) { //if the user want to limit the number of colors
+			return function(data) { //set up a function
+				return posterizeImage(data, Math.pow(maxColors_, 0.3333333)); //to posterize the image
+			};
+		})(maxColors);
+
+		getPixelsFromImage(textImgSrc, textImgWidth, function(data) { //then get its image
 			allImagePixels.push(data); //and add its pixels to the pixels array
 			numLeftToLoad -= 1;
 			if (numLeftToLoad == 0) callback();
-		});
+		}, 0.5, imageModifier);
 	}
 }
 
@@ -186,11 +203,25 @@ function getSaveFileLink(linkText, fileContents) {
 		   '</a>';
 }
 
-function getPixelsFromImage(location, width, aspectRatioMultiplier, callback) { //returns array of pixel colors in the image
+function posterizeImage(data, posterizeValue) {
+	var v = 256/posterizeValue;
+	for (var ai = 0; ai < data.length; ai+=4) { //go through its pixels
+		var red = data[ai+0];
+		var green = data[ai+1];
+		var blue = data[ai+2];
+		data[ai+0] = Math.floor(red/v)*v - 1;
+		data[ai+1] = Math.floor(green/v)*v - 1;
+		data[ai+2] = Math.floor(blue/v)*v - 1;
+	}
+
+	return data;
+}
+
+function getPixelsFromImage(location, width, callback, aspectRatioMultiplier, modifier) { //returns array of pixel colors in the image
 	var timeStartedGettingPixels = new Date().getTime();
 	var img = new Image(); //make a new image
 	img.onload = function() { //when it is finished loading
-		var aspectRatio = aspectRatioMultiplier*(img.height/img.width);
+		var aspectRatio = aspectRatioMultiplier*(img.height/img.width); //calculate the aspect ratio
 		var canvas = document.createElement('canvas'); //make a canvas element
 		canvas.width = width; //with this width
 		canvas.height = width*aspectRatio; //and this height (keep it proportional)
@@ -200,6 +231,11 @@ function getPixelsFromImage(location, width, aspectRatioMultiplier, callback) { 
 		ctx.drawImage(img, 0, 0, width, width*aspectRatio); //so that you can draw the image
 		var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height); //and grab its pixels
 		document.body.removeChild(canvas); //all done, so get rid of it
+
+		if (modifier != null) { //if they want to modify the image
+			imageData.data = modifier(imageData.data); //then change the image data
+			ctx.putImageData(imageData, 0, 0); //and put the new pixels on the canvas
+		}
 
 		callback(imageData.data, new Date().getTime() - timeStartedGettingPixels); //...all so you can send the pixels (and the time taken to get them) back through the callback
 	};
